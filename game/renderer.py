502ac5d189c63f0e96850f4c3a23c18469199e12
Tran Dinh Khoa <trandinhkhoa1803@gmail.com>
2026-07-19 09:25:26 +0700

    Update renderer.py

--- game/renderer.py
+++ game/renderer.py
@@ -184,95 +184,95 @@ class BlockRenderer:
             visible=False,
         )
 
-    def sync_with_state(self, state: GameState) -> None:
-        if state.is_split:
-            # Hiện khối bị động
-            self.inactive_outline.visible = True
-            self.inactive_entity.visible = True
+    # def sync_with_state(self, state: GameState) -> None:
+    #     if state.is_split:
+    #         # Hiện khối bị động
+    #         self.inactive_outline.visible = True
+    #         self.inactive_entity.visible = True
 
-            cubes = state.split_cubes
-            active_idx = state.active_cube
-            inactive_idx = 1 - active_idx
+    #         cubes = state.split_cubes
+    #         active_idx = state.active_cube
+    #         inactive_idx = 1 - active_idx
 
-            active_pos = cubes[active_idx]
-            inactive_pos = cubes[inactive_idx]
+    #         active_pos = cubes[active_idx]
+    #         inactive_pos = cubes[inactive_idx]
 
-            # 1. Vẽ khối chủ động
-            inner = Vec3(0.76, 0.76, 0.76)
-            outline = inner + Vec3(0.06, 0.06, 0.06)
+    #         # 1. Vẽ khối chủ động
+    #         inner = Vec3(0.76, 0.76, 0.76)
+    #         outline = inner + Vec3(0.06, 0.06, 0.06)
 
-            self.outline.scale = outline
-            self.entity.scale = inner
+    #         self.outline.scale = outline
+    #         self.entity.scale = inner
 
-            pos_active = Vec3(active_pos[1], self.TILE_TOP_Y + 0.38, -active_pos[0])
-            self.outline.position = pos_active
-            self.entity.position = pos_active
+    #         pos_active = Vec3(active_pos[1], self.TILE_TOP_Y + 0.38, -active_pos[0])
+    #         self.outline.position = pos_active
+    #         self.entity.position = pos_active
 
-            # 2. Vẽ khối bị động
-            self.inactive_outline.scale = outline
-            self.inactive_entity.scale = inner
+    #         # 2. Vẽ khối bị động
+    #         self.inactive_outline.scale = outline
+    #         self.inactive_entity.scale = inner
 
-            pos_inactive = Vec3(inactive_pos[1], self.TILE_TOP_Y + 0.38, -inactive_pos[0])
-            self.inactive_outline.position = pos_inactive
-            self.inactive_entity.position = pos_inactive
+    #         pos_inactive = Vec3(inactive_pos[1], self.TILE_TOP_Y + 0.38, -inactive_pos[0])
+    #         self.inactive_outline.position = pos_inactive
+    #         self.inactive_entity.position = pos_inactive
 
-        else:
-            # Ẩn khối bị động
-            self.inactive_outline.visible = False
-            self.inactive_entity.visible = False
+    #     else:
+    #         # Ẩn khối bị động
+    #         # self.inactive_outline.visible = False
+    #         # self.inactive_entity.visible = False
 
-            block = state.block
+    #         # block = state.block
 
-            if block.orientation == Orientation.STANDING:
-                inner = Vec3(0.76, 2.0, 0.76)
-                outline = inner + Vec3(0.06, 0.06, 0.06)
+    #         if block.orientation == Orientation.STANDING:
+    #             inner = Vec3(0.76, 2.0, 0.76)
+    #             outline = inner + Vec3(0.06, 0.06, 0.06)
 
-                self.outline.scale = outline
-                self.entity.scale = inner
+    #             self.outline.scale = outline
+    #             self.entity.scale = inner
 
-                pos = Vec3(block.col, self.TILE_TOP_Y + 1.0, -block.row)
-                self.outline.position = pos
-                self.entity.position = pos
+    #             pos = Vec3(block.col, self.TILE_TOP_Y + 1.0, -block.row)
+    #             self.outline.position = pos
+    #             self.entity.position = pos
 
-            elif block.orientation == Orientation.HORIZONTAL:
-                inner = Vec3(2.0, 0.76, 0.76)
-                outline = inner + Vec3(0.06, 0.06, 0.06)
+    #         elif block.orientation == Orientation.HORIZONTAL:
+    #             inner = Vec3(2.0, 0.76, 0.76)
+    #             outline = inner + Vec3(0.06, 0.06, 0.06)
 
-                self.outline.scale = outline
-                self.entity.scale = inner
+    #             self.outline.scale = outline
+    #             self.entity.scale = inner
 
-                pos = Vec3(block.col + 0.5, self.TILE_TOP_Y + 0.38, -block.row)
-                self.outline.position = pos
-                self.entity.position = pos
+    #             pos = Vec3(block.col + 0.5, self.TILE_TOP_Y + 0.38, -block.row)
+    #             self.outline.position = pos
+    #             self.entity.position = pos
 
-            elif block.orientation == Orientation.VERTICAL:
-                inner = Vec3(0.76, 0.76, 2.0)
-                outline = inner + Vec3(0.06, 0.06, 0.06)
+    #         elif block.orientation == Orientation.VERTICAL:
+    #             inner = Vec3(0.76, 0.76, 2.0)
+    #             outline = inner + Vec3(0.06, 0.06, 0.06)
 
-                self.outline.scale = outline
-                self.entity.scale = inner
+    #             self.outline.scale = outline
+    #             self.entity.scale = inner
 
-                pos = Vec3(block.col, self.TILE_TOP_Y + 0.38, -(block.row + 0.5))
-                self.outline.position = pos
-                self.entity.position = pos
+    #             pos = Vec3(block.col, self.TILE_TOP_Y + 0.38, -(block.row + 0.5))
+    #             self.outline.position = pos
+    #             self.entity.position = pos
 
-            elif block.orientation == Orientation.CUBE:
-                inner = Vec3(0.76, 0.76, 0.76)
-                outline = inner + Vec3(0.06, 0.06, 0.06)
+    #         elif block.orientation == Orientation.CUBE:
+    #             inner = Vec3(0.76, 0.76, 0.76)
+    #             outline = inner + Vec3(0.06, 0.06, 0.06)
 
-                self.outline.scale = outline
-                self.entity.scale = inner
+    #             self.outline.scale = outline
+    #             self.entity.scale = inner
 
-                pos = Vec3(block.col, self.TILE_TOP_Y + 0.38, -block.row)
-                self.outline.position = pos
-                self.entity.position = pos
+    #             pos = Vec3(block.col, self.TILE_TOP_Y + 0.38, -block.row)
+    #             self.outline.position = pos
+    #             self.entity.position = pos
 
-            else:
-                raise ValueError(
-                    f"Unsupported orientation: {block.orientation}"
-                )
+    #         else:
+    #             raise ValueError(
+    #                 f"Unsupported orientation: {block.orientation}"
+    #             )
 
-        self.entity.rotation = Vec3(0, 0, 0)
+    #     self.entity.rotation = Vec3(0, 0, 0)
 
     def destroy(self) -> None:
         destroy(self.entity)
